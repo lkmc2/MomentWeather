9000307a920a51628fb51f692e3d974ebde060e4
lkmc2 <lkmc2@163.com>
2017-12-02 23:15:29 +0800

    Update CitiesTemperature.js

--- src/components/city/CitiesTemperature.js
+++ src/components/city/CitiesTemperature.js
@@ -11,10 +11,13 @@ import {
     FlatList,
     ScrollView,
 } from 'react-native';
-import WeeklyTemperature from '../weekly/WeeklyTemperature.js';
-import WeeklyDate from "../weekly/WeeklyDate";
-import CityList from "./CityList";
-import DateItem from "./DateItem";
+import WeeklyTemperature from '../weekly/WeeklyTemperature.js'; //每周天气控件
+import WeeklyDate from "../weekly/WeeklyDate.js"; //每周日期控件
+import CityList from "./CityList.js"; //城市列表控件
+import DateItem from "./DateItem.js"; //日期显示控件
+import WeatherStore from '../../stores/WeatherStore.js'; //天气存储数据库
+import StateStore from '../../stores/StateStore.js'; //天气状态数据库
+import {observer} from 'mobx-react/native';
 
 const title = [
     {day: '周二', date: '11.22'},
@@ -69,50 +72,60 @@ const datas = [
 ];
 
 //城市温度控件
+
+
+@observer
 export default class CitiesTemperature extends Component {
 
-    render() {
+    renderWeeklyTempList = (weatherData) => {
+        if (weatherData !== null && weatherData.length > 0) {
+            return weatherData.map(data =>
+                <FlatList
+                    data={data.daily_forecast}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    scrollEnabled={true}
+                    renderItem={
+                        ({item, index}) => {
+                            if (index > 4) return null;
+                            return (
+                                <WeeklyTemperature
+                                    weatherCode={item.cond_code_d}
+                                    maxTemp={item.tmp_max}
+                                    minTemp={item.tmp_min}
+                                />
+                            )
+                        }
 
-        let renderWeeklyTempList = datas.map(data =>
-            <FlatList
-                data={data}
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                scrollEnabled={true}
-                renderItem={
-                    ({item, index}) => {
-                        if (index > 4) return null;
-                        return (
-                            <WeeklyTemperature
-                                icon={item.icon}
-                                maxTemp={item.maxTemp}
-                                minTemp={item.minTemp}
-                            />
-                        )
                     }
+                />
+            );
+        }
+    };
 
-                }
-            />
-        );
+    renderTitle = (weatherData) => {
+        if (weatherData !== null && weatherData.length > 0) {
+            return weatherData[0].daily_forecast.map(item =>
+                <WeeklyDate date={item.date}/>
+            );
+        }
+    };
 
-        let renderTitle = title.map(item =>
-            <WeeklyDate
-                day={item.day}
-                date={item.date}/>
-        );
+    render() {
+        const weatherData = StateStore.cityDataSource; //获取城市数据
 
         return (
             <View
                 style={styles.container}>
                 <View style={styles.rowView}>
                     <DateItem/>
-                    {renderTitle}
+                    {this.renderTitle(weatherData)}
                 </View>
                 <ScrollView>
                     <View style={styles.rowView}>
                         <CityList style={styles.cityList}/>
                         <View style={styles.wrapperView}>
-                            {renderWeeklyTempList}
+                            {this.renderWeeklyTempList(weatherData)}
                         </View>
                     </View>
                 </ScrollView>
