1e7837076e45db1204b3d380b1e45d184be495c5
lkmc2 <lkmc2@163.com>
2017-12-03 13:23:48 +0800

    Add EditCityPage.js and EditCityItem.js and EditCityList.js

---
+++ src/components/edit/EditCityItem.js
@@ -0,0 +1,32 @@
+/**
+ * Created by lkmc2 on 2017/12/3.
+ */
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    FlatList,
+} from 'react-native';
+import IconUtil from '../../util/IconUtil.js'; //图标工具类
+
+export default class EditCityPage extends Component {
+    render() {
+
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffb4a2',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
